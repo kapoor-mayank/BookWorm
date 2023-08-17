0fe72a289a35048792db41250e45fd87e195382b
Satish <Satish@gmail.com>
2023-08-17 15:56:06 +0530

    Satish Commit

--- src/main/java/com/example/entities/BeneficiaryMaster.java
+++ src/main/java/com/example/entities/BeneficiaryMaster.java
@@ -18,67 +18,7 @@ public class BeneficiaryMaster {
     private String benContactNo;
     private String benBankName;
     private String benBankBranch;
-    public Long getBenId() {
-		return benId;
-	}
-	public void setBenId(Long benId) {
-		this.benId = benId;
-	}
-	public String getBenName() {
-		return benName;
-	}
-	public void setBenName(String benName) {
-		this.benName = benName;
-	}
-	public String getBenEmailId() {
-		return benEmailId;
-	}
-	public void setBenEmailId(String benEmailId) {
-		this.benEmailId = benEmailId;
-	}
-	public String getBenContactNo() {
-		return benContactNo;
-	}
-	public void setBenContactNo(String benContactNo) {
-		this.benContactNo = benContactNo;
-	}
-	public String getBenBankName() {
-		return benBankName;
-	}
-	public void setBenBankName(String benBankName) {
-		this.benBankName = benBankName;
-	}
-	public String getBenBankBranch() {
-		return benBankBranch;
-	}
-	public void setBenBankBranch(String benBankBranch) {
-		this.benBankBranch = benBankBranch;
-	}
-	public String getBenIFSC() {
-		return benIFSC;
-	}
-	public void setBenIFSC(String benIFSC) {
-		this.benIFSC = benIFSC;
-	}
-	public String getBenAccNo() {
-		return benAccNo;
-	}
-	public void setBenAccNo(String benAccNo) {
-		this.benAccNo = benAccNo;
-	}
-	public String getBenAccType() {
-		return benAccType;
-	}
-	public void setBenAccType(String benAccType) {
-		this.benAccType = benAccType;
-	}
-	public String getBenPAN() {
-		return benPAN;
-	}
-	public void setBenPAN(String benPAN) {
-		this.benPAN = benPAN;
-	}
-	private String benIFSC;
+    private String benIFSC;
     private String benAccNo;
     private String benAccType;
     private String benPAN;
