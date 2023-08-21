418d376ced7495f9a6296802c4cec4065cffdd53
67051_Piyush <piyushpatil001@gmail.com>
2023-08-21 12:10:14 +0530

    mayur language_master

---
+++ src/main/java/com/example/controllers/entities/LanguageMaster.java
@@ -0,0 +1,53 @@
+package com.example.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class LanguageMaster {
+
+    @Override
+	public String toString() {
+		return "LanguageMaster [languageId=" + languageId + ", languageDesc=" + languageDesc + ", productType="
+				+ productType + "]";
+	}
+
+	public Long getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(Long languageId) {
+		this.languageId = languageId;
+	}
+
+	public String getLanguageDesc() {
+		return languageDesc;
+	}
+
+	public void setLanguageDesc(String languageDesc) {
+		this.languageDesc = languageDesc;
+	}
+
+	public ProductTypeMaster getProductType() {
+		return productType;
+	}
+
+	public void setProductType(ProductTypeMaster productType) {
+		this.productType = productType;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long languageId;
+    
+    private String languageDesc;
+    
+    @OneToOne
+    @JoinColumn(name = "typeId")
+    private ProductTypeMaster productType;
+    
+    // Getter and setter methods
+}
